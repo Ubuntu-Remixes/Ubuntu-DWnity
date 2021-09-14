6f398c86eab4b15cc36814e82ac788f7dbf37a8b
Rudra Saraswat <rs2009@ubuntu.com>
2021-09-14 17:10:54 +0000

    Update remix.sh

--- build/remix.sh
+++ build/remix.sh
@@ -3,4 +3,5 @@
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 
-apt-get install -y ubuntu-mate-desktop plymouth-theme-ubuntu-mate-logo ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu-mate
+apt-get install -y terminator dwm ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
+apt-get purge -y unity-greeter
