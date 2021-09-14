f1ddee522896c03d9d6ac20c233785b43b81508e
Rudra Saraswat <rs2009@ubuntu.com>
2021-09-14 18:13:37 +0530

    Add Ubuntu MATE changes

--- build/remix.sh
+++ build/remix.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
 add-apt-repository -y --no-update universe
-add-apt-repository -y --no-update multiverse
+add-apt-repository -y multiverse
 
-add-apt-repository -y ppa:ubuntucinnamonremix/unstable
-
-apt-get install -y casper expect gparted ubuntucinnamon-desktop \
- cifs-utils lupin-casper language-pack-en language-pack-gnome-en \
- xfsprogs jfsutils reiserfsprogs shim-signed shim ntfs-3g lvm2 \
- dmraid wamerican fwupdate mokutil \
- zram-config ubiquity ubiquity-slideshow-ubuntu ubiquity-frontend-gtk
+apt-get install -y ubuntu-mate-desktop plymouth-theme-ubuntu-mate-logo ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu-mate
