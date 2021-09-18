a4bc5ead4ac76534881460bb75993e13ea335238
Fuseteam <fusekai@outlook.com>
2021-09-18 01:36:25 -0300

    add personal builds of suckless utils

--- build/remix.sh
+++ build/remix.sh
@@ -3,5 +3,17 @@
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 
-apt-get install -y byobu git kde-connect terminator dwm ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
+apt-get install -y byobu git kde-connect ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
 apt-get purge -y unity-greeter
+
+cd /usr/local/src
+git clone https://gitlab.com/dwnity/fust
+cd fust
+make clean install
+cd ..
+git clone https://github.com/dwnity/dfuse
+cd dfuse
+make clean install
+cd ..
+git clone https://github.com/dwnity/fusedwm
+make clean install
