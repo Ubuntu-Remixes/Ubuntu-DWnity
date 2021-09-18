e72da7fab67227b51b05eafe2ca9d2cbdcad78ea
Fuseteam <fusekai@outlook.com>
2021-09-17 23:27:12 -0300

    add kdeconnect and byobu

--- build/remix.sh
+++ build/remix.sh
@@ -3,5 +3,5 @@
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 
-apt-get install -y terminator synapse dwm ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
+apt-get install -y byobu kde-connect terminator dwm ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
 apt-get purge -y unity-greeter
