baf6c4e31dbb81de9b6bc11ed8c8055aab53f15d
Fuseteam <fusekai@outlook.com>
2021-09-19 02:40:32 -0300

    fix apt install and install make

--- build/remix.sh
+++ build/remix.sh
@@ -3,7 +3,7 @@
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 
-apt-get install -y byobu git kde-connect ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
+apt-get install -y byobu make git kdeconnect ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
 apt-get purge -y unity-greeter
 
 cd /usr/local/src
