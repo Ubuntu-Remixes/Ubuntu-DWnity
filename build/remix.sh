1a25be8ed07aaaee15322e0b560f512f5358cb53
Rahammetoela Toekiman <fusekai@outlook.com>
2021-09-23 14:49:50 +0000

    add flatpak support

--- build/remix.sh
+++ build/remix.sh
@@ -4,7 +4,9 @@ add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 add-apt-repository -y ppa:appimagelauncher-team/stable
 
-apt-get install -y gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
+apt-get install -y gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
 apt-get purge -y unity-greeter
+
+flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 bash suckless.sh
