2e9ae93e4baf1afc8b1b9efe104e6cb9affa6a38
Rahammetoela Toekiman <fusekai@outlook.com>
2021-09-23 14:48:21 +0000

    add appimage launcher

--- build/remix.sh
+++ build/remix.sh
@@ -2,8 +2,9 @@
 
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
+add-apt-repository -y ppa:appimagelauncher-team/stable
 
-apt-get install -y gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
+apt-get install -y gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
 apt-get purge -y unity-greeter
 
 bash suckless.sh
