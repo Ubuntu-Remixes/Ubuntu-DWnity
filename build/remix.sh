04acfb348870dda2ea3061c0b6e2475016a8fad0
Fuseteam <fusekai@outlook.com>
2021-09-25 12:07:44 -0300

    re-add dwm as apt package and add mlocate

--- build/remix.sh
+++ build/remix.sh
@@ -4,7 +4,7 @@ add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 add-apt-repository -y ppa:appimagelauncher-team/stable
 
-apt-get install -y gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
+apt-get install -y byobu mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kde-connect ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
 apt-get purge -y unity-greeter
 
 flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
