3dcaa30b5e2a0d23a8c9976b63fd02761f865738
Fuseteam <fusekai@outlook.com>
2021-09-28 14:35:19 -0300

    restructure dwnity into a subgroup of tuxecure + add fusemoji

--- build/suckless.sh
+++ build/suckless.sh
@@ -1,13 +1,17 @@
 cd /usr/local/src
-git clone https://gitlab.com/dwnity/fust
+git clone https://gitlab.com/tuxecure/dwnity/fust
 cd fust
 make clean install
 cd ..
-git clone https://gitlab.com/dwnity/dfuse
+git clone https://gitlab.com/tuxecure/dwnity/dfuse
 cd dfuse
 git submodule update --init --recursive
 make clean install
 cd ..
-git clone https://gitlab.com/dwnity/fusedwm
+git clone https://gitlab.com/tuxecure/dwnity/fusedwm
 cd fusedwm
 make clean install
+cd ..
+git clone https://gitlab.com/tuxecure/dwnity/fusemoji
+cd fuseemoji
+make install
