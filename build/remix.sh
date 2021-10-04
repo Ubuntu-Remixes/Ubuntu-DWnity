510957dfd2b1ed6f746f198f9d44f06a5d792049
Rahammetoela Toekiman <fusekai@outlook.com>
2021-10-04 15:47:31 +0000

    gnome-getting-started-docs is not installed

--- build/remix.sh
+++ build/remix.sh
@@ -13,10 +13,8 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 913558C8A5E552A7
 apt-get update
 
 apt-get install -y libpam-usb synapse sshfs vim mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter ubuntu-unity-desktop yaru-unity7 ubuntu-unity-settings ubuntu-unity-backgrounds plymouth-theme-ubuntu-unity
-apt-get purge -y gnome-initial-setup gnome-control-center gnome-calendar gnome-characters gnome-control-center-data \
-gnome-control-center-faces gnome-desktop3-data \
-gnome-font-viewer gnome-getting-started-docs \
-gnome-initial-setup gnome-keyring gnome-keyring-pkcs11 gnome-logs \
+apt-get purge -y gnome-initial-setup gnome-control-center gnome-calendar gnome-characters gnome-control-center-data gnome-control-center-faces gnome-desktop3-data \
+gnome-font-viewer gnome-initial-setup gnome-keyring gnome-keyring-pkcs11 gnome-logs \
 gnome-mahjongg gnome-menus gnome-mines gnome-online-accounts \
 gnome-screenshot gnome-session-bin gnome-session-canberra \
 gnome-session-common gnome-settings-daemon gnome-settings-daemon-common \
