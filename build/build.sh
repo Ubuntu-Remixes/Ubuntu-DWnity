d1eaf8e7e4353a8a80f116695cd1d2d9fe5e5287
Rahammetoela Toekiman <fusekai@outlook.com>
2021-10-04 13:00:06 +0000

    install ubuntu-unity-desktop and optimize build.sh

--- build/build.sh
+++ build/build.sh
@@ -14,24 +14,6 @@ mv "/boot/initrd.fromiso" "/boot/initrd.img-$KERNEL"
 mv "/boot/vmlinuz.fromiso" "/boot/vmlinuz-$KERNEL"
 cd /build/
 
-# Remve GNOME stuff
-for pkg in gnome-calculator gnome-calendar \
-gnome-characters gnome-control-center gnome-control-center-data \
-gnome-control-center-faces gnome-desktop3-data \
-gnome-font-viewer gnome-getting-started-docs \
-gnome-initial-setup gnome-keyring gnome-keyring-pkcs11 gnome-logs \
-gnome-mahjongg gnome-menus gnome-mines gnome-online-accounts \
-gnome-power-manager gnome-screenshot gnome-session-bin gnome-session-canberra \
-gnome-session-common gnome-settings-daemon gnome-settings-daemon-common \
-gnome-shell gnome-shell-common gnome-shell-extension-appindicator \
-gnome-shell-extension-desktop-icons gnome-shell-extension-ubuntu-dock \
-gnome-startup-applications gnome-sudoku gnome-system-monitor gnome-terminal \
-gnome-terminal-data gnome-themes-extra gnome-themes-extra-data gnome-todo \
-gnome-todo-common gnome-user-docs gnome-video-effects \
-nautilus-extension-gnome-terminal pinentry-gnome3 yaru-theme-gnome-shell;
-do apt-get purge -y $pkg; done
-apt-get install -y software-properties-gtk
-
 # Run the remix script
 bash remix.sh && apt-get autoremove -y --purge
 
--- build/remix.sh
+++ build/remix.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+# Remve GNOME stuff
+apt-get remove ubuntu-desktop
+apt-get autoremove -y
+apt-get install -y software-properties-gtk
 
+add-apt-repository -y ppa:~ubuntu-unity-devs/stable
 add-apt-repository -y --no-update universe
 add-apt-repository -y multiverse
 add-apt-repository -y ppa:appimagelauncher-team/stable
@@ -7,8 +12,7 @@ echo "deb https://apt.mcdope.org/ ./" > /etc/apt/sources.list.d/mcdope.list
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 913558C8A5E552A7
 apt-get update
 
-apt-get install -y libpam-usb synapse sshfs vim mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter
-apt-get purge -y unity-greeter
+apt-get install -y libpam-usb synapse sshfs vim mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter ubuntu-unity-desktop yaru-unity7 ubuntu-unity-settings ubuntu-unity-backgrounds plymouth-theme-ubuntu-unity
 
 flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
