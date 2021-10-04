b3f62923c7d00e9150176143d61f1d52dc1d3e41
Rahammetoela Toekiman <fusekai@outlook.com>
2021-10-04 19:00:14 +0000

    remove nemo

--- build/remix.sh
+++ build/remix.sh
@@ -13,7 +13,7 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 913558C8A5E552A7
 apt-get update
 
 apt-get install -y libpam-usb synapse sshfs vim mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter ubuntu-unity-desktop yaru-unity7 ubuntu-unity-settings ubuntu-unity-backgrounds plymouth-theme-ubuntu-unity
-apt-get purge -y gnome-software* gnome-control-center
+apt-get purge -y gnome-software* gnome-control-center nemo*
 
 flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
