b3cc6f2b431f8eb976b099139aa7207b24172d64
Rahammetoela Toekiman <fusekai@outlook.com>
2021-10-04 19:59:09 +0000

    remove more packages

--- build/remix.sh
+++ build/remix.sh
@@ -12,8 +12,8 @@ echo "deb https://apt.mcdope.org/ ./" > /etc/apt/sources.list.d/mcdope.list
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 913558C8A5E552A7
 apt-get update
 
-apt-get install -y libpam-usb synapse sshfs vim mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter ubuntu-unity-desktop yaru-unity7 ubuntu-unity-settings ubuntu-unity-backgrounds plymouth-theme-ubuntu-unity
-apt-get purge -y gnome-software* gnome-control-center nemo*
+apt-get install -y vlc libpam-usb synapse sshfs vim mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter ubuntu-unity-desktop yaru-unity7 ubuntu-unity-settings ubuntu-unity-backgrounds plymouth-theme-ubuntu-unity
+apt-get purge -y gnome-software* gnome-control-center nemo* rhythmbox* totem* libreoffice* geary* gedit* shotwell* picom* transmission*
 
 flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
