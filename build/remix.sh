f774733ebb3d1a564036bc349c95153325d771f7
Rahammetoela Toekiman <fusekai@outlook.com>
2021-10-04 16:54:07 +0000

    remove more before installing unity and less after

--- build/remix.sh
+++ build/remix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Remve GNOME stuff
-apt-get remove -y ubuntu-desktop gdm3 ubuntu-session
+apt-get remove -y ubuntu-desktop* gdm3 ubuntu-session
 apt-get autoremove -y
 apt-get install -y software-properties-gtk
 
@@ -13,12 +13,11 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 913558C8A5E552A7
 apt-get update
 
 apt-get install -y libpam-usb synapse sshfs vim mlocate make git dwm gcc libxft-dev libx11-dev libxinerama-dev compton nitrogen scrot byobu make git kdeconnect appimagelauncher flatpak ubiquity ubiquity-frontend-gtk ubiquity-slideshow-ubuntu lightdm lightdm-gtk-greeter ubuntu-unity-desktop yaru-unity7 ubuntu-unity-settings ubuntu-unity-backgrounds plymouth-theme-ubuntu-unity
-apt-get purge -y gnome-initial-setup gnome-control-center gnome-calendar gnome-characters gnome-control-center-data gnome-control-center-faces gnome-desktop3-data \
-gnome-font-viewer gnome-initial-setup gnome-keyring gnome-keyring-pkcs11 gnome-logs \
+apt-get purge -y gnome-initial-setup gnome-control-center gnome-calendar gnome-characters gnome-desktop3-data \
+gnome-font-viewer gnome-keyring gnome-keyring-pkcs11 gnome-logs \
 gnome-mahjongg gnome-menus gnome-mines gnome-online-accounts \
 gnome-screenshot gnome-session-bin gnome-session-canberra \
-gnome-session-common gnome-settings-daemon gnome-settings-daemon-common \
-gnome-shell gnome-shell-common gnome-shell-extension-appindicator gnome-shell-extension-ubuntu-dock \
+gnome-session-common gnome-settings-daemon gnome-shell gnome-shell-common gnome-shell-extension-appindicator gnome-shell-extension-ubuntu-dock \
 gnome-startup-applications gnome-sudoku gnome-terminal \
 gnome-terminal-data gnome-themes-extra gnome-themes-extra-data gnome-todo \
 gnome-todo-common gnome-user-docs gnome-video-effects \
